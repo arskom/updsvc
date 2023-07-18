78c37ba6f8abc75363a56221c52080717f6703d1
sevvaldiriarin <sevvaldiriarin@gmail.com>
2023-07-18 15:49:55 +0300

    test temp file path

--- SvcTest.cpp
+++ SvcTest.cpp
@@ -7,13 +7,26 @@ int main(int argc, char *argv[]) {
     if (!version.empty())
     {
         std::cout << "Program Version: " << version << std::endl;
-    }*/
+    }
 
     // UpdateDetector(CreateRequest());
-    std::string domain, path, url;
-    url = "https://ampmail.net/release/mgui-wgt/mgui-wgt-4.1.93-x64_mgui-wgt-4.1.92-x64.exe";
-    domain = urlSplit(0, url);
-    path = urlSplit(1, url);
-    std::cout << "domain:" << domain << "    path:" << path << std::endl;
+    std::string url =
+            "https://ampmail.net/release/mgui-wgt/mgui-wgt-4.1.93-x64_mgui-wgt-4.1.92-x64.exe";
+
+    std::string domain, path, filename;
+    urlSplit(url, domain, path, filename);
+
+    std::cout << "Domain: " << domain << std::endl;
+    std::cout << "Path: " << path << std::endl;
+    std::cout << "Filename: " << filename << std::endl;*/
+
+    const char *tempDir = std::getenv("TEMP");
+    if (tempDir == nullptr) {
+        std::cerr << "Failed to retrieve the temporary directory path." << std::endl;
+    }
+    std::string filename = "mgui-wgt-4.1.93-x64_mgui-wgt-4.1.92-x64.exe";
+    std::string tempFilePath = std::string(tempDir) + "\\" + filename;
+    std::cout << "file path: " << tempFilePath << std::endl;
+
     return 0;
 }
