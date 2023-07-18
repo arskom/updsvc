42890c919af8912a1182e865e3c5857743dccebe
sevvaldiriarin <sevvaldiriarin@gmail.com>
2023-07-18 10:44:39 +0300

    test url split function

--- SvcTest.cpp
+++ SvcTest.cpp
@@ -10,5 +10,10 @@ int main(int argc, char *argv[]) {
     }*/
 
     // UpdateDetector(CreateRequest());
+    std::string domain, path, url;
+    url = "https://ampmail.net/release/mgui-wgt/mgui-wgt-4.1.93-x64_mgui-wgt-4.1.92-x64.exe";
+    domain = urlSplit(0, url);
+    path = urlSplit(1, url);
+    std::cout << "domain:" << domain << "    path:" << path << std::endl;
     return 0;
 }
