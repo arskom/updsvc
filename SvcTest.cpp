57045a8f9b11419e9f9baf47533d6bffdf63c01f
sevvaldiriarin <sevvaldiriarin@gmail.com>
2023-07-20 16:37:10 +0300

    test getsourcepath function

--- SvcTest.cpp
+++ SvcTest.cpp
@@ -4,14 +4,13 @@
 
 int main(int argc, char *argv[]) {
 
-    const wchar_t *msiPath =
-            L"C:\\ProgramData\\Package "
-            L"Cache\\{028818E2-5DF4-414F-A1E4-2AA542DE4697}v4.1.92\\mgui-wgt-4-x64.msi";
+    auto source = GetSourcePath();
+    auto a = GetFirstFileNameInDirectory(source);
+    auto fullpath = source + a;
+    const wchar_t *msiPath = fullpath.c_str();
     std::wstring directoryParent, defaultDir;
     if (ReadMSI(msiPath, directoryParent, defaultDir)) {
         // Extraction successful
-        // Now you can use directoryParent and defaultDir as needed
-        // For example, you can print their values:
         wprintf(L"Directory_Parent: %s\n", directoryParent.c_str());
         wprintf(L"DefaultDir: %s\n", defaultDir.c_str());
     }
