1e3a5e7d149bae1410b1ea8032292a0584816b00
sevvaldiriarin <sevvaldiriarin@gmail.com>
2023-08-02 18:02:19 +0300

    implementation of getting guid as argument

--- Settings/Write.cpp
+++ Settings/Write.cpp
@@ -12,12 +12,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine,
     int argc = 0;
     LPWSTR *argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 
-    std::wstringstream converter(argv[1]);
+    std::wstringstream converter(argv[2]);
     DWORD dwordValue;
     converter >> dwordValue;
-
-    WriteRegistryDWORD(L"SOFTWARE\\Arskom\\updsvc", L"PERIOD", dwordValue);
-    WriteRegistryString(L"SOFTWARE\\Arskom\\updsvc", L"REL_CHAN", argv[2]);
+    std::wstring guid = argv[1];
+    WriteRegistryDWORD(L"SOFTWARE\\Arskom\\updsvc\\" + guid, L"PERIOD", dwordValue);
+    WriteRegistryString(L"SOFTWARE\\Arskom\\updsvc\\" + guid, L"REL_CHAN", argv[3]);
 
     return 0;
 }
